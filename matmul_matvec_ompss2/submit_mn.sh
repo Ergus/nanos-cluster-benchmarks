a3a717464f2743b96db2b05d7b9a493ff907a68f
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2021-01-26 22:02:22 +0100

    Simplify submitter.

--- matmul_matvec_ompss2/submit_mn.sh
+++ matmul_matvec_ompss2/submit_mn.sh
@@ -7,98 +7,45 @@
 
 source @PROJECT_BINARY_DIR@/argparse.sh
 add_argument -a x -l exe -h "Executable file" -t file
-add_argument -a R -l rows -h "Rows" -t int
-add_argument -a C -l cols -h "Columns" -t int
-add_argument -a b -l bsize -h "Block size" -t int
-add_argument -a i -l iter -h "Inner Repetitions in task default[1]" -t int
-add_argument -a p -l print -h "Print matrix default" -t int
-add_argument -a I -l Iter -h "Repetitions per program default[1]" -t int
-add_argument -a o -l output -h "Output directory (used only for extrae)" -d ""
-add_argument -a n -l nanos -h "Nanos version to use"
-add_argument -a s -l scheduler -h "Nanos scheduler" -t string
-add_argument -a m -l mpi -h "MPI version" -t string
+add_argument -a R -l repeats -h "Repetitions per program default[1]" -t int
+
 parse_args "$@"
 printargs >&2
 
-module purge
-module load bsc/1.0
-module load gcc/7.2.0
+dims=(1024 4096 8192)
+blocksizes=(64 128 256)
 
-if [[ ${ARGS[m]} == "impi" ]]; then
-	module load hwloc/1.11.8
-	module load intel/2018.1 impi/2018.1 mkl/2018.1
-	module load EXTRAE/3.7.1
-	module load boost/1.64.0-mpi
-	module load nanos6/cluster
-	module load mcxx/git_impi
-elif [[ ${ARGS[m]} == "ompi" ]]; then
-	module load openmpi/3.1.1
-	module load EXTRAE/3.6.1
-	module load nanos6/cluster_ompi
-	module load mcxx/git_ompi
-else
-	echo "ERROR: You need to specify impi or ompi as first argument"
-fi
+REPEATS=${ARGS[R]}
 
+# Start run here printing run info header
+echo "# Job: ${SLURM_JOB_NAME} id: ${SLURM_JOB_ID}"
+echo "# Nodes: ${SLURM_JOB_NUM_NODES} Tasks_per_Node: ${SLURM_NTASKS_PER_NODE} Cores_per_node: ${SLURM_JOB_CPUS_PER_NODE}"
+echo "# Nodes_List: ${SLURM_JOB_NODELIST}"
+echo "# QOS: ${SLURM_JOB_QOS}"
+echo "# Account: ${SLURM_JOB_ACCOUNT} Submitter_host: ${SLURM_SUBMIT_HOST} Running_Host: ${SLURMD_NODENAME}"
 
-ulimit -c unlimited
-
-# Environment variables
-export NANOS6_CPU_SCHEDULER=fifo
-export NANOS6_COMMUNICATION=mpi-2sided
-export NANOS6_DISTRIBUTED_MEMORY=128G
-export NANOS6_LOCAL_MEMORY=8G
-
-export NANOS6_SCHEDULER=${ARGS[s]}
-export NANOS6=${ARGS[n]}
-
-iterations=${ARGS[I]}
-
-COMMAND="${ARGS[x]} ${ARGS[R]} ${ARGS[C]} ${ARGS[b]} ${ARGS[i]} ${ARGS[p]}"
-
-# If EXTRAE
-if [[ ${NANOS6} = extrae ]]; then
-
-	dest="${ARGS[o]}/${SLURM_JOB_NAME}"
-	export EXTRAE_CONFIG_FILE="${dest}.xml"
-	iterations=1  # with extrae enabled only 1 iteration is allowed
-
-	# Create xml for this for this benchmark if extrae is set
-	sed -e "s|PREFIX|${SLURM_JOB_NAME}|"    \
-	    -e "s|EXTRAEHOME|${EXTRAE_HOME}|"   \
-	    -e "s|TMPDIR|${dest}_tmp|"          \
-	    -e "s|FINALDIR|${ARGS[o]}|"         \
-	    -e "s|BINARY|${ARGS[x]}|"           \
-	    -e "s|NPR|${SLURM_JOB_NUM_NODES}|"  \
-	    extrae_template.xml > ${EXTRAE_CONFIG_FILE}
-
-	export NANOS6_EXTRAE_AS_THREADS=1
-	#export LD_PRELOAD="${EXTRAE_HOME}/lib/libnanosmpitrace.so"
-
-	#echo -e "# Enabled EXTRAE with EXTRAE_CONFIG_FILE=${EXTRAE_CONFIG_FILE}"
-	#echo -e "# LD_PRELOAD=${LD_PRELOAD}"
-
-	COMMAND="./trace.sh ${COMMAND}"
-fi
-
-# Start run here
-echo -e "# Job: ${SLURM_JOB_NAME} id: ${SLURM_JOB_ID}"
-echo -e "# Nodes: ${SLURM_JOB_NUM_NODES} Tasks_per_Node: ${SLURM_NTASKS_PER_NODE} Cores_per_node: ${SLURM_JOB_CPUS_PER_NODE}"
-echo -e "# Nodes_List: ${SLURM_JOB_NODELIST}"
-echo -e "# QOS: ${SLURM_JOB_QOS}"
-echo -e "# Account: ${SLURM_JOB_ACCOUNT} Submitter_host: ${SLURM_SUBMIT_HOST} Running_Host: ${SLURMD_NODENAME}"
-echo -e "# Iterations: ${iterations}"
-echo -e "# Command: " ${COMMAND}
+echo "# Repetitions: ${REPEATS}"
 env | grep NANOS6 | sed -e 's/^#*/# /'
-echo -e "# ======================================\n"
+echo "# ======================================"
 
-for ((it=0; it<iterations; ++it))
-    {
-	    echo "# Starting it: ${it} at: " $(date)
-	    start=$SECONDS
-	    srun ${COMMAND}
-	    end=$SECONDS
-	    echo "# Ending: " $(date)
-	    echo "# Elapsed: "$((end-start))
-	    echo -e "\n# --------------------------------------\n"
-    }
+for dim in ${dims[@]}; do
+	for bs in ${blocksizes[@]}; do
+		if [ $((SLURM_JOB_NUM_NODES*bs<=dim)) = 1 ]; then
+			COMMAND="${ARGS[x]} $dim $bs 5"
+			echo -e "# Starting command: ${COMMAND}"
+			echo "# ======================================"
+			for ((it=0; it<${REPEATS}; ++it)) {
+				echo "# Starting it: ${it} at: $(date)"
+				start=${SECONDS}
+				srun ${COMMAND}
+				end=${SECONDS}
+				echo "# Ending:  $(date)"
+				echo "# Elapsed: $((end-start))"
+				echo "# --------------------------------------"
+			}
+			echo ""
+		else
+			echo "# Jump combination nodes: $node, dim: $dim, bs: $bs"
+		fi
+	done
+done
--- matmul_matvec_ompss2/submiter_mn.sh
+++ matmul_matvec_ompss2/submiter_mn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2019  Jimmy Aguilar Mena
+# Copyright (C) 2021  Jimmy Aguilar Mena
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,79 +17,35 @@
 
 source @PROJECT_BINARY_DIR@/argparse.sh
 add_argument -a x -l exe -h "Executable file" -t file
-add_argument -a i -l iter -h "Inner Repetitions default[1]" -t int -d 1
-add_argument -a p -l print -h "Print matrices default[true]" -t bool -d 1
-add_argument -a I -l Iter -h "Repetitions per program default[1]" -t int -d 1
-add_argument -a w -l wtime -h "Wall time limit for jobs" -t timer -d 00:10:00
-add_argument -a q -l queue -h "queue" -t enum -e "debug bsc_cs xlarge" -d "debug"
+add_argument -a w -l wtime -h "Wall time limit for jobs" -t timer -d 01:00:00
+add_argument -a q -l queue -h "queue" -t enum -e "debug bsc_cs xlarge" -d "bsc_cs"
+add_argument -a R -l repeats -h "Repetitions per program default[1]" -t int -d 5
 
-add_argument -a N -l Nodes -h "List of nodes numbers" -t string -d 4
-add_argument -a d -l dimensions -h "List of dimensions" -t string -d 128
-add_argument -a b -l bsizes -h "List block sizes" -t string -d 8
-
-add_argument -a t -l type -h "Type of scalability experiment to test" -t enum\
-	     -d "strong" -e "weak strong"
-
-add_argument -a n -l nanos -h "Nanos version to use" -t enum\
-	     -e "Debug optimized extrae verbose-debug" -d "debug"
-add_argument -a s -l scheduler -h "Nanos scheduler" -t enum\
-	     -e "cluster-random cluster-locality" -d "cluster-random"
-add_argument -a m -l mpi -h "MPI version to use" -t enum\
-	     -e "impi ompi" -d "impi"
-
-#-e "distributed distributedLocation distributedLocationBalanced"\
 parse_args "$@"
 printargs
 
-IFS=', ' read -r -a nodes <<< "${ARGS[N]}"
-IFS=', ' read -r -a blocksize <<< "${ARGS[b]}"
-IFS=', ' read -r -a dims <<< "${ARGS[d]}"
-
 now=$(date +%F_%H-%M-%S)
-name=${ARGS[x]/%.nanos6}
-resdir="results/${name}_${ARGS[n]}_${ARGS[s]}_${ARGS[t]}_${ARGS[p]}_${ARGS[i]}_${now}"
+name=$(basename ${ARGS[x]})
+resdir="results/${name}_${now}"
 
 mkdir -p ${resdir}
 
-echo "Directory: ${resdir}"
-echo "Submitting: group"
+echo "Output: ${resdir}"
+
+nodes=(1 2 4 8)
 echo "nodes: ${nodes[*]}"
-echo "blocks: ${blocksize[*]}"
-echo "dim: ${dims[*]}"
 
-for dim in ${dims[@]}; do
-	for node in ${nodes[@]}; do
+for node in ${nodes[@]}; do
+	echo "Submitting nodes: ${node}"
 
-		case ${ARGS[t]} in
-			strong) rows=${dim};;
-			weak) rows=$(( dim * node ));;
-			*) echo "Error in experiment type";;
-		esac
+	jobname="${name}_${node}"
+	filename="${resdir}/${jobname}"
 
-		for bs in ${blocksize[@]}; do
-
-			if [ $((node*bs<=dim)) = 1 ]; then
-				printf "Submitting combination dim: %s bs: %s nodes: %s\n" \
-					   $dim $bs $node
-				jobname="mv_${dim}_${bs}_${node}"
-				filename="${resdir}/${jobname}"
-
- 				sbatch --ntasks=${node} \
-				       --time=${ARGS[w]} \
-				       --qos=${ARGS[q]} \
- 				       --job-name=${jobname} \
- 				       --output="${resdir}/%x_%2a_%j.out" \
- 				       --error="${resdir}/%x_%2a_%j.err" \
- 				       ./submit_mn.sh -x ${ARGS[x]} \
-				       -R ${rows} -C ${dim} -b ${bs} \
-				       -i ${ARGS[i]} \
-				       -p $([[ ${ARGS[p]} = true ]] && echo 1 || echo 0) \
-				       -I ${ARGS[I]} -o ${resdir} \
-				       -n ${ARGS[n]} -s ${ARGS[s]} -m ${ARGS[m]}
-			else
-				printf "Jump combination dim: %s bs: %s nodes: %s\n" \
-				       $dim $bs $node
-			fi
-		done
-	done
+ 	sbatch --ntasks=${node} \
+		   --time=${ARGS[w]} \
+		   --qos=${ARGS[q]} \
+ 		   --job-name=${jobname} \
+ 		   --output="${resdir}/%x_%2a_%j.out" \
+ 		   --error="${resdir}/%x_%2a_%j.err" \
+ 		   ./submit_mn.sh -R ${ARGS[R]} -x ${ARGS[x]}
 done
