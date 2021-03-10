dfea91f0d4b81c90275a07ec60461e012e150230
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2021-03-10 06:48:24 +0100

    Add group graphs.

--- utils/graph.py
+++ utils/graph.py
@@ -21,6 +21,7 @@ import os
 import json
 import pandas as pd
 import numpy as np
+import re
 
 data = {}
 
@@ -32,15 +33,28 @@ def load_file(input_file):
     # data[filename] = json.load(input_file)
     print("Loading: ", filename)
     data[filename] = pd.DataFrame(json.load(input_file))
-    print(data[filename])
-
-    # print(data[filename][data[filename]['Tasksize'] == 32])
-    # print(data[filename]['Tasksize'].drop_duplicates())
-    # row_vals = data[filename]['Rows'].drop_duplicates().to_list()
-
-    # print(data[filename]['Rows'].drop_duplicates().to_list())
 
     return filename
+
+def add_lines(ax1, ax2, dt_ts, column, label):
+    assert dt_ts.empty == False
+    x = dt_ts['worldsize']
+
+    row_one = dt_ts[x == 1]
+
+    one = row_one[column].values[0] / row_one['Iterations'].values[0]
+
+    # Time graph
+    dt_ts_its = dt_ts['Iterations']
+    y=dt_ts[column].divide(dt_ts_its)
+    err=dt_ts[column+' stdev'].divide(dt_ts_its * dt_ts['executions']**1/2)
+    ax1.errorbar(x, y, err, fmt ='o-', label=label)
+
+    # Scalability
+    sy = one / y
+    erry = sy * err / y
+    ax2.errorbar(x, sy, erry, fmt ='o-', label=label)
+
 
 def process_tasksize(key, column):
     """Create graphs time vs tasksize"""
@@ -66,22 +80,12 @@ def process_tasksize(key, column):
 
         # Time
         for ts in tasksize_vals:
-
             dt_ts = dt_rows[dt_rows['Tasksize'] == ts]
+            if dt_ts.empty:
+                print("Ignore Tasksize: ", ts)
+                continue
 
-            row_one = dt_ts[dt_ts['worldsize'] == 1]
-            one = row_one[column].values[0] / row_one['Iterations'].values[0]
-
-            # Time graph
-            x=dt_ts['worldsize']
-            y=dt_ts[column].divide(dt_ts['Iterations'])
-            err=dt_ts[column+' stdev'].divide(dt_ts['Iterations']*dt_ts['executions']**1/2)
-            ax1.errorbar(x, y, err, fmt ='o-', label=str(ts))
-
-            # Scalability
-            sy = one / y
-            erry = sy * err / y
-            ax2.errorbar(x, sy, erry, fmt ='o-', label=str(ts))
+            add_lines(ax1, ax2, dt_ts, column, str(ts))
 
         plt.legend(loc='center right',
                    bbox_to_anchor=(1.14, 1),
@@ -133,6 +137,66 @@ def process_bars_tasksize(key):
         plt.close()
         print("Generated: ", filename)
 
+def process_group(dim, ts, keys, prefix):
+    print("Processing group: ", dim, ts)
+    fig = plt.figure()
+    gs = fig.add_gridspec(2, hspace=0)
+    (ax1, ax2) = gs.subplots(sharex=True, sharey=False)
+    fig.suptitle(str(dim)+"x"+str(ts))
+
+    #First Graph (Time)
+    ax1.set_xlabel('Nodes')
+    ax1.set_ylabel("Algorithm time")
+    ax1.grid(color='b', ls = '-.', lw = 0.25)
+
+    #Second Graph (Scalability)
+    ax2.set_xlabel('Nodes')
+    ax2.set_ylabel("Scalability ")
+    ax2.grid(color='b', ls = '-.', lw = 0.25)
+
+    for key in keys:
+        if key.find("strong") != -1:
+            tag = "strong"
+        elif key.find("weak") != -1:
+            split = key.split("_")
+            tag = split[2]+" "+split[4]
+        elif key.find("mpi") != -1:
+            tag = "mpi"
+
+        dt = data[key]
+        dt_ts = dt.loc[(dt['Rows'] == dim) & (dt['Tasksize'] == ts)]
+
+        if dt_ts.empty:
+            print("Ignore key %s, Rows: %s, Tasksize: %s"
+                  % (key, dim, ts))
+            continue
+
+        add_lines(ax1, ax2, dt_ts, "Algorithm time", tag)
+
+    plt.legend(loc='center right',
+               bbox_to_anchor=(1.14, 2),
+               fancybox=True, shadow=True, ncol=1)
+
+    filename = prefix+"_"+str(dim)+"_"+str(ts)+".png"
+    plt.savefig(filename)
+    plt.close()
+    print("Generated: ", filename)
+
+
+def process_all(prefix, myregex):
+    dt = next(iter(data.values()))
+
+    row_vals = dt['Rows'].drop_duplicates()   # number of rows tests.
+    ts_vals = dt['Tasksize'].drop_duplicates()   # number of rows tests.
+
+    # Iterate over keys.
+    re1 = re.compile(myregex)    # Comments like # Anything
+    keys = [i for i in data if re1.search(i)]
+
+    for dim in row_vals:
+        for ts in ts_vals:
+            process_group(dim, ts, keys, prefix)
+
 
 if __name__ == "__main__":
     for fname in sys.argv[1:]:
@@ -145,6 +209,10 @@ if __name__ == "__main__":
 
         except IOError:
             print("File not accessible")
+
+
+    process_all("Compare1", "mpi|(matvec_weak_fetchfirst)|(matvec_strong)")
+    process_all("Compare2", "(matvec_weak)|(matvec_strong)")
 
     # print(json.dumps(data, indent=4))
 
