b3ff340a50403935c45c77d16f0ca5642433fcdf
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2021-03-27 23:25:56 +0100

    Improve trace.sh

--- utils/trace.sh
+++ utils/trace.sh
@@ -2,9 +2,22 @@
 
 export EXTRAE_ON=1
 export EXTRAE_CONFIG_FILE=extrae.xml
-export LD_PRELOAD=${EXTRAE_HOME}/lib/libnanosmpitrace.so
 
-export NANOS6_CONFIG=@PROJECT_BINARY_DIR@/nanos6.toml
+export NANOS6_CONFIG=/gpfs/projects/bsc28/bsc28860/nanos6-cluster-benchmarks/build_segfault/nanos6.toml
 export NANOS6_CONFIG_OVERRIDE="version.debug=false,version.instrument="extrae",cluster.disable_remote=false"
 
-./$@
+# Remove previous traces
+if  [ -z ${MPI_LOCALRANKID} ] || [ $MPI_LOCALRANKID -eq 0 ]; then
+	echo "Deleting old traces"
+	rm -rf TRACE.* set-0/ *.prv *.row *.pcf
+fi
+
+export LD_PRELOAD=${EXTRAE_HOME}/lib/libnanosmpitrace.so
+$@
+unset LD_PRELOAD
+
+# Create the mpits
+if  [ -z ${MPI_LOCALRANKID} ] || [ $MPI_LOCALRANKID -eq 0 ]; then
+	echo "Creating mpits file manually"
+	ls ${PWD}/set-0/*.mpit | sed 's/$/ named/' > TRACE.mpits
+fi
