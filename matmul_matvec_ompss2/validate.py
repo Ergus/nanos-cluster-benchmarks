a7af8b5a52bb12f72eb9939aea654b7bec87d206
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2021-05-06 02:19:37 +0200

    Improve validator in matvec and matmul

--- matmul_matvec_ompss2/validate.py
+++ matmul_matvec_ompss2/validate.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-# Copyright (C) 2019  Jimmy Aguilar Mena
+# Copyright (C) 2021  Jimmy Aguilar Mena
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,21 +15,31 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
 import numpy as np
 
-A = np.loadtxt("matrix_A.mat")
-x = np.loadtxt("vector_x.mat")
-b = np.loadtxt("vector_b.mat")
+if __name__ == "__main__":
 
-b2 = np.dot(A, x)
+    A = np.loadtxt(sys.argv[1])
+    print("A = ", A.shape)
+    B = np.loadtxt(sys.argv[2])
+    print("B = ", B.shape)
+    C1 = np.loadtxt(sys.argv[3])
+    print("C = ", C1.shape)
 
-norm = np.max(np.abs(b2 - b))
+    C2 = np.dot(A, B)
 
-print ("Max difference %g" % norm)
+    norm = np.max(np.abs(C2 - C1))
 
-if (norm > 1.0e-3):
-    i = 0
-    for xb, xb2 in zip(b, b2):
-        if np.abs(xb - xb2) > 1.0e-3:
-            print ("i = %d b[i] = %g b2[i] = %g" % (i, xb, xb2))
-        i += 1
+    if (norm > 1.0e-3):
+        print("Error")
+        i = 0
+        for xb, xb2 in zip(C1, C2):
+            if np.abs(xb - xb2) > 1.0e-3:
+                print ("i = %d b[i] = %g b2[i] = %g" % (i, xb, xb2))
+                i += 1
+    else:
+        print("Ok")
+
+    print("Max difference: %g" % norm)
+
