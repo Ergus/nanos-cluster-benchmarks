6838629ef93d05597565635334da44ec2edb65fe
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2022-01-11 00:19:31 +0100

    Improve process script regex uses.

--- utils/process_dim.py
+++ utils/process_dim.py
@@ -22,101 +22,84 @@ from statistics import mean, stdev
 
 import json
 
-re_ignore = re.compile('(# [^-=]+)|(^performance)')    # Comments like # Anything
-re_pair = re.compile('(?P<key>\w+): (?P<value>.+)')   # KEY: value
+# Comments like: # Anything
+re_comment = re.compile('^#(?P<next> -+$)?(?P<report> =+$)?')
 
-re_number = re.compile('(?P<number>\d+(?P<float>\.\d+(e\+\d+)?)?)') # KEY: number
+re_pair = re.compile('^(?P<key>\w+): (?P<value>.+)$')   # KEY: value
+re_number = re.compile('^(?P<number>\d+(?P<float>\.\d+(e\+\d+)?)?)$') # KEY: number
+re_string = re.compile('^"(?P<string>.+?)"$') # KEY: "string"
 
-re_next = re.compile('# -+')            # Divisor like # ---------
-re_report = re.compile('# =+')          # Divisor like # =========
 
 results = {}
 
 def process_group(a_dict):
     """Process group of executions with same parameters."""
 
-    copydic = {}
+    copydic :dict = {}
     for key in a_dict:
         assert(isinstance(a_dict[key], list))
-        vals_list = a_dict[key]
-        count = len(vals_list)
-        assert count > 0
+        vals_list :list = a_dict[key]
+        count :int = len(vals_list)
+        assert count > 0, "List shouldn't be empty"
 
         if "executions" in copydic:
-            assert copydic["executions"] == count
+            assert copydic["executions"] == count, "Unmatched array size"
         else:
             copydic["executions"] = count
 
         # keys int and string
         if all(isinstance(i, (str, int)) for i in vals_list) :
-            if all(i == vals_list[0] for i in vals_list):
-                copydic[key] = vals_list[0]
-            else:
-                print(vals_list)
-                sys.exit("Failed `all` instances!")
-
-        else:  # Floats
-            if count == 1: # single element.
-                assert isinstance(vals_list[0], float)
-                m = vals_list[0]
-                s = 0;
-            else:          # Use mean and stdev when multiple elements
-                assert all(isinstance(i, (float, int)) for i in vals_list)
-                m = mean(vals_list)
-                s = stdev(vals_list)
-
-            copydic[key] = m
-            copydic[key + "_stdev"] = s
+            assert all(i == vals_list[0] for i in vals_list), "Failed `all' instances!"
+            copydic[key] = vals_list[0]
+        elif all(isinstance(i, float) for i in vals_list) :  # Floats
+            copydic[key] = mean(vals_list)
+            copydic[key + "_stdev"] = stdev(vals_list) if count > 1 else 0
+        else:
+            sys.exit("List with mixed or unknown types: " + str(vals_list))
 
     exe = copydic.pop("Executable")
     assert exe
     basename = os.path.basename(exe)
     results.setdefault(basename, []).append(copydic)
 
+
 def process_file(input_file):
     """Process the files and print the data in json format."""
 
-    line_dict = {}
-    count = 0
+    line_dict :dict = {}
+    count :int = 0
 
     for line in input_file:
-        # Ignore
-        if re_ignore.match(line):
+        if match := re_comment.match(line): # Commented lines
+            if match.group('next'):         # -------------- repetition
+                count = count + 1
+            elif match.group('report'):     # ============== end group
+                if count > 0:
+                    process_group(line_dict)
+                    line_dict = {}
+                    count = 0
             continue
 
         # A pair value is always attached.
-        # Even the filename for latter check
-        pair = re_pair.match(line)
-        if pair:
-            key = pair.groupdict()['key']
-            value = pair.groupdict()['value']
+        if pair := re_pair.match(line):
+            key :str = pair.group('key')
+            strvalue :str = pair.group('value')
 
-            number = re_number.match(value)
-            if number:
-                if number.groupdict()['float']: # it is a float so will be averaged later
-                    value = float(number.groupdict()['number'])
-                else:                           # it is a key so will be used as a key/info
-                    value = int(number.groupdict()['number'])
+            if match := re_number.match(strvalue):
+                if match.group('float'):              # it is a float so will be averaged later
+                    value :float = float(match.group('number'))
+                else:                                  # it is a key so will be used as a key/info
+                    value :int = int(match.group('number'))
+            elif match := re_string.match(strvalue):
+                value :str = match.group('string')    # remove the " around string
             else:
-                value = value.strip('\"')
+                sys.exit("Value type with unknown regex: " + str(strvalue))
+
             # Create or append
             line_dict.setdefault(key, []).append(value)
             continue
 
-        # -------------- repetition
-        if re_next.match(line):
-            count = count + 1
-            continue
-
-        # ============== end group
-        if re_report.match(line):
-            if count > 0:
-                process_group(line_dict)
-                line_dict = {}
-                count = 0
-            continue
-
-    if count > 0:
+    if count > 0:  # Lines ended, so this is the end hook
         process_group(line_dict)
 
 
@@ -126,16 +109,14 @@ if __name__ == "__main__":
             print("Going into:", dirname)
             results = {}
             # Process input
-            for basename_in in os.listdir(dirname):
-                if basename_in.endswith(".out"):
-                    fname_in = os.path.join(dirname, basename_in)
-                    print("Processing:", fname_in)
-                    try:
-                        with open(fname_in) as fin:
-                            process_file(fin)
-                    except IOError:
-                        print("Couldn't read input:", fname_in)
-
+            for basename_in in [f for f in os.listdir(dirname) if f.endswith(".out")]:
+                fname_in = os.path.join(dirname, basename_in)
+                print("Processing:", fname_in)
+                try:
+                    with open(fname_in) as fin:
+                        process_file(fin)
+                except IOError:
+                    print("Couldn't read input:", fname_in)
 
             # Write output
             fname_out = dirname.rstrip(os.sep)+".json"
