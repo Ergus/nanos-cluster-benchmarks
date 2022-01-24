381afa7c8374271fe2c876a62c64f80dd02bdea0
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2022-01-24 19:08:33 +0100

    Replace old graph script.

--- utils/graph.py
+++ utils/graph.py
@@ -16,214 +16,243 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import matplotlib.pyplot as plt
-import sys
-import os
+import sys, os
 import json
 import pandas as pd
 import numpy as np
-import re
+import pprint
+import pickle
+from typing import *
 
-data = {}
 
-def load_file(input_file):
-    """Process the files and print the data in json format."""
-    basename = os.path.basename(input_file.name)
-    filename = os.path.splitext(basename)[0]
+def save_all_files(filename: str, fig):
+    """Save the graphs to two files."""
 
-    # data[filename] = json.load(input_file)
-    print("Loading: ", filename)
-    data[filename] = pd.DataFrame(json.load(input_file))
+    # Save the plots with pickle to recover them:
+    #
+    # import matplotlib.pyplot as plt
+    # import pickle
+    # with open('filename.pkl', 'rb') as pkl:
+    #     ax = pickle.load(pkl)
+    # plt.show()
+    with open(filename + ".pkl",'wb') as pkl:
+        pickle.dump(fig, pkl)
 
-    return filename
+    # Save the image as a png
+    fig.savefig(filename + ".png",
+                dpi=300,
+                format='png',
+                #bbox_extra_artists=[leg],
+                bbox_inches='tight')
 
-def add_lines(ax1, ax2, dt_ts, column, label):
-    assert dt_ts.empty == False
-    dt_ts = dt_ts.sort_values(by=['worldsize'])
 
-    x = dt_ts['worldsize']
-
-    # Time graph
-    y=dt_ts[column]
-    err=dt_ts[column+' stdev'].divide(dt_ts['executions']**1/2)
-    ax1.errorbar(x, y, err, fmt ='o-', label=label)
-
-    fact = 1
-    if dt_ts[x == fact].empty:
-        fact = 2
-
-    if dt_ts[x == fact].empty:
-        print (" Can't create scalability graph for: ", label)
+def add_lines_and_scale(ax1, ax2, dt_ts , column : str, label: str):
+    """Add lines to the graphs."""
+    if dt_ts.empty:
+        print("Ignoring: ", label)
         return
 
-    one = dt_ts[x == fact][column].values[0]
+    dt_sorted = dt_ts.sort_values(by=['worldsize'])
+    x = dt_sorted['worldsize']
+
+    row_one = dt_sorted[x == 1]
+    if row_one.empty:
+        print("No single node for : ", label)
+        return
+
+    # Time graph
+    y = dt_sorted[column]
+    erry = dt_sorted[column+'_stdev'].divide(dt_sorted['executions']**(1/2))
+
+    ax1.errorbar(x, y, erry, fmt ='o-', label=label)
+
+    one : float = row_one[column].values[0]
+    errone : float = (row_one[column+'_stdev'] / (row_one['executions']**(1/2))).values[0]
 
     # Scalability
-    sy = one / y
-    erry = sy * err / y
-    ax2.errorbar(x, sy, erry, fmt ='o-', label=label)
+    sy = one / dt_sorted[column]
+    errsy = sy * (erry/y + errone/one)
+
+    ax2.errorbar(x, sy, errsy, fmt ='o-', label=label)
 
 
-def process_tasksize(key, column):
+def process_tasksize(data, keyslist:list, rows:int, ts:int, cpu_count:int, column:str):
     """Create graphs time vs tasksize"""
-    print("Call process_tasksize(%s, %s)" % (key, column))
-    dt = data[key]
-    row_vals = dt['Rows'].drop_duplicates()   # number of rows tests.
 
-    for rows in row_vals:
-        dt_rows = dt[dt['Rows'] == rows]
-        tasksize_vals = dt_rows['Tasksize'].drop_duplicates()
-
-        fig = plt.figure()
-        gs = fig.add_gridspec(2, hspace=0)
-        (ax1, ax2) = gs.subplots(sharex=True, sharey=False)
-        fig.suptitle(key + " dim: " + str(rows))
-
-        ax1.set_xlabel('Nodes')
-        ax1.set_ylabel(column)
-        ax1.grid(color='b', ls = '-.', lw = 0.25)
-
-        ax2.set_xlabel('Nodes')
-        ax2.set_ylabel("Scalability "+column)
-        ax2.grid(color='b', ls = '-.', lw = 0.25)
-
-        # Time
-        for ts in tasksize_vals:
-            dt_ts = dt_rows[dt_rows['Tasksize'] == ts]
-            if dt_ts.empty:
-                print(" Ignore Tasksize: ", ts)
-                continue
-
-            add_lines(ax1, ax2, dt_ts, column, str(ts))
-
-        plt.legend(loc='center right',
-                   bbox_to_anchor=(1.14, 1),
-                   fancybox=True, shadow=True, ncol=1)
-
-        filename = column.replace(" ", "_")+"_"+key+"_"+str(rows)+".png"
-        plt.savefig(filename)
-        plt.close()
-        print(" Generated: ", filename)
-
-def process_bars_tasksize(key):
-    """Create graphs time vs tasksize"""
-    print("Call process_bars_tasksize(%s)" % (key))
-    dt = data[key]
-    ts_vals = dt['Tasksize'].drop_duplicates()   # number of rows tests.
-
-    for ts in ts_vals:
-        dt_ts = dt[dt['Tasksize'] == ts]   # rows with tasksize == ts
-
-        row_vals = dt_ts['Rows'].drop_duplicates()
-
-        # Figure
-        fig, ax = plt.subplots()
-
-        fig.suptitle("Tasksize " + str(ts))
-
-        ax.set_xlabel('Nodes')
-        ax.set_ylabel('Time')
-        ax.grid(axis = 'y', color = 'b', ls = '-.', lw = 0.25)
-
-        nbars = len(row_vals)
-        width = 1.0 / (nbars + 1)
-        offset = - width * nbars / 4 # /2 center /2 half
-
-        for rows in row_vals:
-            dt_rows = dt_ts[dt_ts['Rows'] == rows]
-
-            x = np.arange(dt_rows['worldsize'].size)
-
-            ax.bar(x + offset, dt_rows["Total time"], color = 'b',  width = width)
-            ax.bar(x + offset, dt_rows["Algorithm time"], color = 'r', width = width)
-            offset = offset + width
-
-        x = dt_ts[dt_ts['Rows'] == rows]['worldsize']
-        ax.set_xticks(np.arange(x.size))
-        ax.set_xticklabels([str(i) for i in x])
-
-        filename = "Bars_"+key+"_"+str(ts)+".png"
-        plt.savefig(filename)
-        plt.close()
-        print(" Generated: ", filename)
-
-def process_group(dim, ts, keys, prefix):
-    print("Processing group: ", dim, ts)
     fig = plt.figure()
     gs = fig.add_gridspec(2, hspace=0)
     (ax1, ax2) = gs.subplots(sharex=True, sharey=False)
-    fig.suptitle(str(dim)+"x"+str(ts))
 
-    #First Graph (Time)
-    ax1.set_xlabel('Nodes')
-    ax1.set_ylabel("Algorithm time")
+    ax1.set_ylabel(column)
     ax1.grid(color='b', ls = '-.', lw = 0.25)
 
-    #Second Graph (Scalability)
     ax2.set_xlabel('Nodes')
-    ax2.set_ylabel("Scalability ")
+    ax2.set_ylabel("Scalability")
     ax2.grid(color='b', ls = '-.', lw = 0.25)
 
-    for key in keys:
-        if key.find("strong") != -1:
-            tag = "strong"
-        elif key.find("weak") != -1:
-            split = key.split("_")
-            tag = split[2]+" "+split[4]
-        elif key.find("mpi") != -1:
-            tag = "mpi"
+    key_prefix : str = keyslist[0].split("_")[0]
+
+    prefix: str = key_prefix if all(key.split("_")[0] == key_prefix for key in keyslist) else ""
+
+    fig.suptitle(prefix + " " + str(rows) + " x " + str(ts) + " (" + str(cpu_count) + " cores)")
+
+    for key in data:
+        dt_key = data[key]
+        # "cholesky_memory_ompss2" -> "memory ompss2"
+        label : str = " ".join(key.split("_")[1:])
+
+        dt = dt_key[(dt_key['Rows'] == rows) & \
+                    (dt_key['Tasksize'] == ts) & \
+                    (dt_key['cpu_count'] == cpu_count)]
+
+        if key.endswith("mpi"):
+            add_lines_and_scale(ax1, ax2, dt, column, label)
         else:
-            printf(" Error: Unknown key type: ", key)
-            continue
+            for ns in range(2):
+                dt_ns = dt[(dt['namespace_enabled'] == ns)]
+                labelns = label + [" nons", " ns"][ns]
 
-        dt = data[key]
-        dt_ts = dt.loc[(dt['Rows'] == dim) & (dt['Tasksize'] == ts)]
+                add_lines_and_scale(ax1, ax2, dt_ns, column, labelns)
 
-        if dt_ts.empty:
-            print("Ignore key %s, Rows: %s, Tasksize: %s"
-                  % (key, dim, ts))
-            continue
-
-        add_lines(ax1, ax2, dt_ts, "Algorithm time", tag)
-
-    plt.legend(loc='center right',
-               bbox_to_anchor=(1.14, 2),
+    plt.legend(bbox_to_anchor=(1,1),
+               loc='center left',
+               fontsize='x-small',
                fancybox=True, shadow=True, ncol=1)
 
-    filename = prefix+"_"+str(dim)+"_"+str(ts)+".png"
-    plt.savefig(filename)
+    # Save image file.
+    filename = "Sacalability_" \
+        + column + "_" \
+        + key.split("_")[0] + "_" \
+        + str(rows) + "_" \
+        + str(ts) + "_" \
+        + str(cpu_count)
+
+    save_all_files(filename, fig)
+
     plt.close()
-    print(" Generated: ", filename)
+    print("Generated: ", filename)
 
 
-def process_all(prefix, myregex):
-    dt = next(iter(data.values()))
+def process_experiment(data, key:str,
+                       rows:int,
+                       ts_vals_list:list[int],
+                       cpu_count:int,
+                       column:str):
+    """Create graphs comparing all the TS for same size and num_cpus"""
 
-    row_vals = dt['Rows'].drop_duplicates()   # number of rows tests.
-    ts_vals = dt['Tasksize'].drop_duplicates()   # number of rows tests.
+    fig = plt.figure()
+    gs = fig.add_gridspec(2, hspace=0)
+    (ax1, ax2) = gs.subplots(sharex=True, sharey=False)
 
-    # Iterate over keys.
-    re1 = re.compile(myregex)    # Comments like # Anything
-    keys = [i for i in data if re1.search(i)]
+    ax1.set_ylabel(column)
+    ax1.grid(color='b', ls = '-.', lw = 0.25)
 
-    for dim in row_vals:
+    ax2.set_xlabel('Nodes')
+    ax2.set_ylabel("Scalability")
+    ax2.grid(color='b', ls = '-.', lw = 0.25)
+
+    # Title
+    fig.suptitle(key + " " + str(rows) + " (" + str(cpu_count) + " cores)")
+
+    dt_key = data[key]
+
+    for ts in ts_vals_list:
+        # "cholesky_memory_ompss2" -> "memory ompss2"
+        label : str = str(ts)
+
+        dt = dt_key[(dt_key['Rows'] == rows) & \
+                    (dt_key['Tasksize'] == ts) & \
+                    (dt_key['cpu_count'] == cpu_count)]
+
+        if key.endswith("mpi"):
+            dt_mpi = dt
+            add_lines_and_scale(ax1, ax2, dt_mpi, column, label)
+        else:
+            for ns in range(2):
+                dt_ns = dt[(dt['namespace_enabled'] == ns)]
+                labelns = label + [" nons", " ns"][ns]
+
+                add_lines_and_scale(ax1, ax2, dt_ns, column, labelns)
+
+    plt.legend(bbox_to_anchor=(1,1),
+               loc='center left',
+               fontsize='x-small',
+               fancybox=True, shadow=True, ncol=1)
+
+    # Save image file.
+    # Save image file.
+    filename = "Compare_" \
+        + column + "_" \
+        + key + "_" \
+        + str(rows) + "_" \
+        + str(cpu_count)
+
+    save_all_files(filename, fig)
+
+    plt.close()
+    print("Generated: ", filename)
+
+
+# Tasksize
+def process_all(data):
+    """Create all the blocksize graphs"""
+
+    keys : list = list(data.keys());
+    first_dt = data[keys[0]]
+
+    # Get all the keys
+    rows_vals : list = first_dt['Rows'].drop_duplicates()
+    ts_vals : list = first_dt['Tasksize'].drop_duplicates()
+    cpu_counts : list = first_dt['cpu_count'].drop_duplicates()
+
+    # All benchmarks
+    for row in rows_vals:
         for ts in ts_vals:
-            process_group(dim, ts, keys, prefix)
+            for cpu_count in cpu_counts:
+                process_tasksize(data, keys, row, ts, cpu_count, "Algorithm_time")
+
+    # All ts for same experiment.
+    for key in keys:
+        for row in rows_vals:
+            for cpu_count in cpu_counts:
+                process_experiment(data, key, row, ts_vals, cpu_count, "Algorithm_time")
 
 
 if __name__ == "__main__":
+    pd.set_option('display.max_rows', None)
+    data : Dict[str, pd.DataFrame] = {}
+
+    if len(sys.argv) < 2:
+        raise ValueError("Needs one filename as argument")
+
     for fname in sys.argv[1:]:
+
+        if fname.split(".")[-1] != "json":
+            raise ValueError("Input file is not a .json")
+
         try:
+            print("Loading:", fname)
+
             with open(fname, 'r') as f:
-                key = load_file(f)
-                process_tasksize(key, "Algorithm_time")
-                process_bars_tasksize(key)
+                fdata = json.load(f)
+
+                for key in fdata:
+                    # key is the experiment name like: cholesky_fare_ompss2_taskfor
+
+                    df_in: pd.DataFrame = pd.DataFrame(fdata[key])  # data to Pandas Dataframe
+
+                    if key in data:
+                        data[key] = data[key].append(df_in)
+                    else:
+                        data[key] = df_in
 
         except IOError:
-            print("File not accessible")
+            print("File not accessible or json corrupt")
 
+    process_all(data)
 
-    process_all("Compare1", "mpi|(matvec_weak_fetchfirst)|(matvec_strong)")
-    process_all("Compare2", "(matvec_weak)|(matvec_strong)")
+    # process_all("Compare1", "mpi|(matvec_weak_fetchfirst)|(matvec_strong)")
+    # process_all("Compare2", "(matvec_weak)|(matvec_strong)")
 
     # print(json.dumps(data, indent=4))
 
